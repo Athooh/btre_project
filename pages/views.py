4d30c11328d663760e32a4e9c7ae5172109c94aa
odhiambo_athooh <s.athooh@gmail.com>
2022-09-23 16:36:17 +0800

    listings content

--- pages/views.py
+++ pages/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', {'navbar': 'index'})
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    return render(request, 'pages/about.html', {'navbar': 'about'})
