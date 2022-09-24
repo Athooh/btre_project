b74d99bb2d61bc7d397519fd7b6ece9fb97b67a2
odhiambo_athooh <s.athooh@gmail.com>
2022-09-25 00:48:39 +0800

    admin logo & css/customize admin display data

--- realtors/admin.py
+++ realtors/admin.py
@@ -2,4 +2,15 @@ from django.contrib import admin
 from .models import Realtor
 
 # Register your models here.
-admin.site.register(Realtor)
+
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'phone', 'email', 'is_mvp', 'hire_date')
+    list_filter = ('name',)
+    list_display_links = ('id', 'name')
+    search_fields = ("name", "phone", "hire_date")
+    #list_editable = ("is_mvp",)
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)
