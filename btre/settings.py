0b2103c087567088ccc365fbcb09445bf36070f1
odhiambo_athooh <s.athooh@gmail.com>
2022-11-01 17:13:48 +0300

    contacts inquiries bundle

--- btre/settings.py
+++ btre/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -146,3 +147,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 's.athooh@gmail.com'
+EMAIL_HOST_PASSWORD = 'lddguqzufketfxki'
+EMAIL_USE_TLS = True
